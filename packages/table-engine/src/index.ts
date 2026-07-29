a030268f5d11c72cc799a972a4788c1b131992af
luyijian <670528353@qq.com>
2026-07-29 11:47:05 +0800

    chore(workspace): expose runtime package entrypoints
    Expose rule-pack browser-safe subpaths and the authoritative TableRuntime for server and table clients.

--- packages/table-engine/src/index.ts
+++ packages/table-engine/src/index.ts
@@ -8,4 +8,5 @@ export * from "./baccarat/draw-table.js";
 export * from "./baccarat/shoe.js";
 export * from "./baccarat/payout.js";
 export * from "./chip-ledger.js";
+export * from "./table-runtime.js";
 export * from "./rng.js";
